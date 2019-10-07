70ff88ea4df7022dd61b808e97f67f93111daeb1
Steve-design <smzalendo31@gmail.com>
2019-10-07 11:33:50 +0300

    add a setUp method and a tearDown method

--- user.py
+++ user.py
@@ -1,3 +1,17 @@
 class User:
 
     user_list =[] 
+
+ def setUp(self):
+
+        '''
+        Set up method to run before each test cases.
+        '''
+ 
+        self.new_user = User("Steve Mwanza", "smzalendo31@gmail.com", "mzalendo") 
+
+    def tearDown(self):
+        '''
+        clean up after each test to prevent errors
+        '''
+        User.user_list = []    
