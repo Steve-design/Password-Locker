dc1a58682740da41e8b6fcb48050e942621cdde1
Steve-design <smzalendo31@gmail.com>
2019-10-07 11:44:45 +0300

    add setUp and tearDown methods, add test for initialization

--- user.py
+++ user.py
@@ -2,25 +2,4 @@ class User:
 
     user_list =[] 
 
- def setUp(self):
-
-        '''
-        Set up method to run before each test cases.
-        '''
- 
-        self.new_user = User("Steve Mwanza", "smzalendo31@gmail.com", "mzalendo") 
-
-    def tearDown(self):
-        '''
-        clean up after each test to prevent errors
-        '''
-        User.user_list = []  
-
-    def test_init(self):
-
-            '''
-            test_init test case to test if the object is initialized properly
-            '''
-            self.assertEqual(self.new_user.user_name,"Steve Mwanza")
-            self.assertEqual(self.new_user.email,"smzalendo31@gmail.com") #Check how initialisation happens as expected.
-            self.assertEqual(self.new_user.password,"mzalendo23")      
+    
--- user_test.py
+++ user_test.py
@@ -1,3 +1,26 @@
 import unittest
 from user import User
 class TestUser(unittest,TestCase):
+
+    def setUp(self):
+
+        '''
+        Set up method to run before each test cases.
+        '''
+ 
+        self.new_user = User("Steve Mwanza", "smzalendo31@gmail.com", "mzalendo") 
+
+    def tearDown(self):
+        '''
+        clean up after each test to prevent errors
+        '''
+        User.user_list = []  
+
+    def test_init(self):
+
+            '''
+            test_init test case for proper object initialization
+            '''
+            self.assertEqual(self.new_user.user_name,"Steve Mwanza")
+            self.assertEqual(self.new_user.email,"smzalendo31@gmail.com")
+            self.assertEqual(self.new_user.password,"mzalendo23")   
