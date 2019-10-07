3d3d331bb44d61f0338f4d0cb229afc86a611c32
Steve-design <smzalendo31@gmail.com>
2019-10-07 10:00:49 +0300

    add a setUp method to define instructions to be executed before each test method

--- credentials_test.py
+++ credentials_test.py
@@ -2,3 +2,10 @@ import unittest
 from credentials import Credentials
 
 class TestCredentials(unittest.TestCase):
+     '''
+    Test Class to test the behaviour of the credentials class
+    '''
+    def setUp(self):    
+        self.new_credentials = Credentials("Facebook", "0713519553", "mzalendo23")
+
+        
