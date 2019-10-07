53cc05f5a5dfb5f23260c068b51cae12dde51f00
Steve-design <smzalendo31@gmail.com>
2019-10-07 11:37:28 +0300

    add an init test case to check that the object is initialized properly

--- user.py
+++ user.py
@@ -14,4 +14,13 @@ class User:
         '''
         clean up after each test to prevent errors
         '''
-        User.user_list = []    
+        User.user_list = []  
+
+    def test_init(self):
+
+            '''
+            test_init test case to test if the object is initialized properly
+            '''
+            self.assertEqual(self.new_user.user_name,"Steve Mwanza")
+            self.assertEqual(self.new_user.email,"smzalendo31@gmail.com") #Check how initialisation happens as expected.
+            self.assertEqual(self.new_user.password,"mzalendo23")      
