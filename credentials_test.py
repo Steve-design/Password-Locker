11b89ddfbde4315f2341c87bd35d1a6468714f33
Steve-design <smzalendo31@gmail.com>
2019-10-07 10:04:45 +0300

    add a test function to initialize the credentials class

--- credentials_test.py
+++ credentials_test.py
@@ -5,7 +5,14 @@ class TestCredentials(unittest.TestCase):
      '''
     Test Class to test the behaviour of the credentials class
     '''
-    def setUp(self):    
+def setUp(self):    
         self.new_credentials = Credentials("Facebook", "0713519553", "mzalendo23")
 
+def test_init(self):
         
+            self.assertEqual(self.new_credentials.account_name, "Facebook",)
+            self.assertEqual(self.new_credentials.login_detail, "0713519553")
+            self.assertEqual(self.new_credentials.Password, "mzalendo23")
+            '''
+            confirms that initialisation of class Credentials happens as expected.Three parameters etc
+            '''    
