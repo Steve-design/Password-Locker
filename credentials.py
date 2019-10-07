4596d8bfb5e80b7439d04d90cb5666cdd55da16e
Steve-design <smzalendo31@gmail.com>
2019-10-07 09:45:36 +0300

    create a new credentials object and pass in all the four credentials

--- credentials.py
+++ credentials.py
@@ -1,2 +1,12 @@
 class Credentials:
      credentials_list = [] 
+
+     def __init__(self, account_name, login_detail , Password):
+    
+        self.account_name = account_name
+        self.login_detail = login_detail
+        self.Password = Password
+        ''' 
+        save our accounts passwords here.
+        '''
+
